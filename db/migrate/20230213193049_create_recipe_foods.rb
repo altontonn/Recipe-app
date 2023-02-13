a597fa79d876c625bf53c074dd36c04e0b436fd6
unknown <ekpenisiraphael@gmail.com>
2023-02-14 01:16:34 +0530

    Foreign key of food and recipe added to recipe table

--- db/migrate/20230213193049_create_recipe_foods.rb
+++ db/migrate/20230213193049_create_recipe_foods.rb
@@ -2,6 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :qulity
+      t.references :recipe, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
 
       t.timestamps
     end
