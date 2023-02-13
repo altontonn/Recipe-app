7e43b1cd5eb840fc2f6ec8188b0dec894b74d925
unknown <ekpenisiraphael@gmail.com>
2023-02-14 01:14:46 +0530

    Foreign key of user added to food table

--- db/migrate/20230213192127_create_foods.rb
+++ db/migrate/20230213192127_create_foods.rb
@@ -1,6 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :name
       t.string :measurement_unit
       t.string :price
