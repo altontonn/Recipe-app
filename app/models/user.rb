0974940439125ce9e6b1a294553d1ca41dddd354
unknown <ekpenisiraphael@gmail.com>
2023-02-14 17:02:11 +0530

    Bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_many :recipes, foreign_key: :user_id
-  has_many :foods,   
-
-  validates :name, presence: true
+  has many :recipes, foreign_key: :user_id
+  has many :foods, foreign_key: :user_id
 end
