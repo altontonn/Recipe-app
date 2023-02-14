af927885260d72edf48c749320c2cdc49529836f
unknown <ekpenisiraphael@gmail.com>
2023-02-14 22:24:31 +0530

    Routes created

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,7 +1,4 @@
 class RecipeFoodsController < ApplicationController
   def index
   end
-
-  def show
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@
 Rails.application.routes.draw do
   root to: 'foods#index'
 
+  resources :recipes, only: [:index, :show] do
+    resources :recipe_foods, only: [:index]
+  end
+  resources :foods, only: [:index, :show]
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
