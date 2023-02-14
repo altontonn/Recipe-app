723fcc1fb05055ab56493b27dc30addf13a76620
Newton <altontonnalumasa@gmail.com>
2023-02-15 01:22:56 +0300

    Resolve the ruby linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :confirmable, :validatable
-  
+
   has_many :recipes, foreign_key: :user_id
   has_many :foods, foreign_key: :user_id
 end
