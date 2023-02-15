dc12a07d141f201dbba576ae633b27f9905a3f1a
unknown <ekpenisiraphael@gmail.com>
2023-02-15 07:11:26 +0530

    instant varable name error reovle

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_215020) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "Name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
