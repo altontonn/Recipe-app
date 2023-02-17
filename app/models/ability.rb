d0e7a6bdee8d715b50cc9c945f5e392aa29a91af
Newton <altontonnalumasa@gmail.com>
2023-02-17 12:28:20 +0300

    fix ruby error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -7,12 +5,12 @@ class Ability
     # Define abilities for the user here. For example:
     #
 
-     # Handle the case where we don't have a current_user i.e. the user is a guest
-     user ||= User.new
+    # Handle the case where we don't have a current_user i.e. the user is a guest
+    user ||= User.new
 
-     # Define a few sample abilities
-     can :manage, Food, user_id: user.id
-     can :manage, Recipe, user_id: user.id
+    # Define a few sample abilities
+    can :manage, Food, user_id: user.id
+    can :manage, Recipe, user_id: user.id
     #   return unless user.present?
     #   can :read, :all
     #   return unless user.admin?
