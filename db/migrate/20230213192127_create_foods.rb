ad34696bf16be3dabcef6af7a6195304779a5fa9
Newton <altontonnalumasa@gmail.com>
2023-02-18 04:20:54 +0300

    Add text indent on migration files

--- db/migrate/20230213192127_create_foods.rb
+++ db/migrate/20230213192127_create_foods.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class CreateFoods < ActiveRecord::Migration[7.0]
-  def change
-    create_table :foods do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :name
-      t.string :measurement_unit
-      t.integer :price
-      t.integer :quantity
+   def change
+      create_table :foods do |t|
+         t.references :user, null: false, foreign_key: true
+         t.string :name
+         t.string :measurement_unit
+         t.integer :price
+         t.integer :quantity
 
-      t.timestamps
-    end
-  end
+         t.timestamps
+      end
+   end
 end
