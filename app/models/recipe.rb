e8daf3aa07ab71caf05f51ddb057c67a5df2debd
Newton <altontonnalumasa@gmail.com>
2023-02-18 04:11:24 +0300

    Increase text indent to three

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,10 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_foods, foreign_key: :recipe_id
+   belongs_to :user
+   has_many :recipe_foods, foreign_key: :recipe_id
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
-  validates :public, presence: true
+   validates :name, presence: true
+   validates :description, presence: true
+   validates :preparation_time, presence: true
+   validates :cooking_time, presence: true
+   validates :public, presence: true
 end
