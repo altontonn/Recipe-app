4d565958313e8e3c19a63adda50a294817b4dec6
Newton <altontonnalumasa@gmail.com>
2023-06-28 23:53:57 +0300

    Create files for render deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,6 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "recipe-app-production-352d.up.railway.app"
-
   # Do not eager load code on boot.
   config.eager_load = false
 
